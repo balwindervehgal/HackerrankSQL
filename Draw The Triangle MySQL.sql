8c21c1c47d01fe3c9d25a47335c64ea7b09502d8
balwindervehgal <36641247+balwindervehgal@users.noreply.github.com>
2018-04-24 12:37:26 -0400

    Update Draw The Triangle MySQL.sql

--- Draw The Triangle MySQL.sql
+++ Draw The Triangle MySQL.sql
@@ -37,3 +37,7 @@ Write a query to print the pattern P(20).
 */
 
 SELECT REPEAT('* ', @NUMBER := @NUMBER - 1) FROM information_schema.tables, (SELECT @NUMBER:=21) t LIMIT 20;
+
+/* To reverse the pattren use below: */
+
+select repeat('* ', @i := @i + 1 ) from information_schema.tables, (SELECT @i := 0) tbl limit 20;
