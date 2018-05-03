6a0b9737a5a1b71a62cf33a0c5766af4d26b4ae8
balwindervehgal <36641247+balwindervehgal@users.noreply.github.com>
2018-05-02 21:45:48 -0400

    Update MySQL dense rank using variable and case statement.sql

--- MySQL dense rank using variable and case statement.sql
+++ MySQL dense rank using variable and case statement.sql
@@ -15,3 +15,12 @@ case when @value = score then @rank else @rank:= @rank+1 end as Ran,
 @value:= score
 from scores, (select @rank:= 0, @value:= NULL)x
 order by score desc
+
+#or
+
+select score,ran
+from (select score, 
+@rank:= IF(@value = score, @rank, @rank+1) as ran ,
+@value := score
+from scores a, (select @rank:= 0, @value:= NULL) x
+order by score desc)s
