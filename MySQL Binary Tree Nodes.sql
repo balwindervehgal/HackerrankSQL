fec1275c83bf8cb0b5c354efe5941cf1cc964239
balwindervehgal <36641247+balwindervehgal@users.noreply.github.com>
2018-05-03 22:42:49 -0400

    Create MySQL Binary Tree Nodes.sql

---
+++ MySQL Binary Tree Nodes.sql
@@ -0,0 +1,22 @@
+/*
+
+You are given a table, BST, containing two columns: N and P, where N represents the value of a node in Binary Tree, and P is the parent of N.
+
+Write a query to find the node type of Binary Tree ordered by the value of the node. Output one of the following for each node:
+
+Root: If node is root node.
+Leaf: If node is leaf node.
+Inner: If node is neither root nor leaf node.
+
+*/
+
+
+
+select N, 'Root' from BST where P is null
+union
+select N, 'Leaf' from BST a where P is not null
+and not exists (select 1 from BST b where a.N = b.P)
+union
+select N, 'Inner' from BST a where P is not null
+and exists (select 1 from BST b where a.N = b.P)
+order by N
